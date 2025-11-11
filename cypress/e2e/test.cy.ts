e54b6a52521cf089e41b27a0fba61b85279f9fb0
xandreiAThome <xandrei37@gmail.com>
2025-11-11 21:01:31 +0800

    test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -7,6 +7,7 @@ describe('test', () => {
     cy.get('#root input[placeholder="Enter your password"]').type('a');
     cy.get('#root button.w-full').click();
     cy.get('#root a[href="/listing"] div:nth-child(2) div.r-userSelect-lrvibr').click();
+    cy.wait(2000);
     cy.get(
       '#root div:nth-child(2) > div.flex-wrap > div:nth-child(1) > div.overflow-hidden > div.w-full'
     ).should('be.visible');
