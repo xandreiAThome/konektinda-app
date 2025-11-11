9c8b2378c833ac7fd0b1e4ea906eedea1effde2e
Eunice Filipino <168982371+EuniceFil@users.noreply.github.com>
2025-11-11 22:39:31 +0800

    test(e2e): add tests for product catalog (#21)
    * test(e2e): add tests for product catalog
    
    ---------
    
    Co-authored-by: xandreiAThome <xandrei37@gmail.com>

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,14 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:8081/counter');
+  it('test', function () {
+    cy.visit('http://localhost:8081');
+    cy.get('#root input[placeholder="Enter your username"]').click();
+    cy.get('#root input[placeholder="Enter your username"]').type('a');
+    cy.get('#root input[placeholder="Enter your password"]').click();
+    cy.get('#root input[placeholder="Enter your password"]').type('a');
+    cy.get('#root button.w-full').click();
+    cy.get('#root a[href="/counter"]').click();
+    cy.get('#root button.bg-primary').click();
+    cy.get('#root button.bg-primary').click();
+    cy.get('#root div.font-bold.text-foreground').should('have.text', '2');
   });
 });
